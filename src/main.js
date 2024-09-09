8febe5e01a63d2c791cdb5e7a17395d73e08cb7b
lantuyan <76894132+lantuyan@users.noreply.github.com>
2024-09-09 14:26:42 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -106,7 +106,17 @@ export default async ({ req, res, log, error }) => {
         });
 
         log('Successfully sent message');
-        
+
+        await databases.updateDocument(
+          buildingDatabaseID,
+          sensorCollectionID,
+          item.$id,
+          {
+            lastNotification: currentDate,
+          }
+        );
+
+        log('Update new lastNotification: '+ currentDate);
       } else {
         log('Do nothing');
         return ;
